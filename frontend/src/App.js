704a1ffb33c37b4616237f51675e6ffd35b6246a
Ajay <majay1638@gmail.com>
2020-08-16 22:13:23 +0530

    designed header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,44 @@
 import React from "react";
 import "./App.scss";
+
+import "./assets/vendor/nucleo/css/nucleo.css";
+import "./assets/scss/argon-design-system-react.scss";
+import "./assets/vendor/font-awesome/css/font-awesome.min.css";
+// import "./assets/scss/argon-design-system-react.scss?v1.1.0";
+import classnames from "classnames";
+// reactstrap components
+import {
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  CardImg,
+  FormGroup,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
 import Header from "./components/header/Header.component";
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <h1>Hello World!!</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {};
+  // componentDidMount() {
+  //   document.documentElement.scrollTop = 0;
+  //   document.scrollingElement.scrollTop = 0;
+  //   this.refs.main.scrollTop = 0;
+  // }
+
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    );
+  }
 }
 
 export default App;
