923e48e8f720b48a5b079c93a0e2ecfedea70620
Ajay <majay1638@gmail.com>
2020-08-16 16:03:50 +0530

    setup fonts, icons,bootstrap

---
+++ frontend/src/components/header/Header.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import reactstrap
+
+// Css
+import "./Header.style.scss";
+
+const Header = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      <i class="fa fa-hourglass-half" aria-hidden="true"></i>
+    </div>
+  );
+};
+
+export default Header;
